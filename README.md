458b40dca695e97aab56087f08b67d51d47fb092
jasonscrouch <jasonscrouch@gmail.com>
2025-10-01 22:54:28 -0500

    WIP Begins to implement Add feature

--- README.md
+++ README.md
@@ -8,9 +8,9 @@ A battle simulation system
 
 # Features
 
-1. Ability to add/remove Transformers
-2. Ability to battle two transformers and get a result
-3. Ability to get win/loss rate for a transformer
+1. Ability to add and remove
+2. Ability to battle any two and get a result
+3. Ability to get win and loss rate
 4. Console or UI display to perform the above actions / display results
 
 # Technologies Used
@@ -37,29 +37,19 @@ for transformers. The system should allow users to add transformers, remove tran
 simulate and display battle results, and display information about transformers.
 System requirements
 
-1. Ability to add/remove Transformers
-2. Ability to battle two transformers and get a result
-3. Ability to get win/loss rate for a transformer
-4. Console or UI display to perform the above actions / display results
-   Additional requirements
-5. Must be implemented in C#
-6. Your “boss” would like the simulation system to support other types of battles in the
-   future.
-   Some implementation guidelines
-7. Implement AT LEAST the following interfaces: (You may have more)
-   o ITransformer: Interface representing a transformer with properties such as name,
-   faction, strength, etc. It MUST include name, number of wins/losses, and faction.
-   o IBattleSimulator – Minimally should have a method to battle two transformers
-   and return a battle result. This can just be a simple random win/lose.
-   o IRepository – Ability to store, update, and retrieve any data object you create
-   (Transformer, Battle results, etc)
-8. Implement concrete classes for the interfaces.
-9. Ensure the Battle Simulator utilizes the repository.
-10. Write a unit test for the battle simulator battle function.
-11. Optionally, you can enhance the system by adding features such as ranking transformers,
-    better simulation etc.
-    Your solution should demonstrate a clear understanding of Object-Oriented principles such as
-    encapsulation, inheritance, and polymorphism. Make use of generics to ensure code reusability
-    and type safety. Additionally, utilize Inversion of Control to decouple dependencies and facilitate
-    easier testing and maintenance. Keep in mind the goals of your “boss” while building your
-    system.
+Additional requirements 6. Your “boss” would like the simulation system to support other types of battles in the
+future.
+
+Some implementation guidelines 7. Implement AT LEAST the following interfaces: (You may have more)
+o ITransformer: Interface representing a transformer with properties such as name,
+faction, strength, etc. It MUST include name, number of wins/losses, and faction.
+o IBattleSimulator – Minimally should have a method to battle two transformers
+and return a battle result. This can just be a simple random win/lose.
+o IRepository – Ability to store, update, and retrieve any data object you create
+(Transformer, Battle results, etc) 8. Implement concrete classes for the interfaces. 9. Ensure the Battle Simulator utilizes the repository. 10. Write a unit test for the battle simulator battle function. 11. Optionally, you can enhance the system by adding features such as ranking transformers,
+better simulation etc.
+Your solution should demonstrate a clear understanding of Object-Oriented principles such as
+encapsulation, inheritance, and polymorphism. Make use of generics to ensure code reusability
+and type safety. Additionally, utilize Inversion of Control to decouple dependencies and facilitate
+easier testing and maintenance. Keep in mind the goals of your “boss” while building your
+system.
--- polemos/Program.cs
+++ polemos/Program.cs
@@ -1,77 +1,202 @@
-﻿namespace polemos;
+﻿using Microsoft.VisualBasic;
 
+namespace polemos;
+
+// todo: move this to UI folder
 internal class Options
 {
-    public const int Add_Combatant = 1;
-    public const int Remove_Combatant = 2;
-    public const int Simulate_Battle = 3;
-    public const int Combatant_Statistics = 4;
+    public const int AddCombatant = 1;
+    public const int RemoveCombatant = 2;
+    public const int SimulateBattle = 3;
+    public const int CombatantStatistics = 4;
     public const int Exit = 5;
+}
+
+// todo: put this in core folder
+internal interface ITransformer : ICombatant
+{
+    string? Faction { get; set; }
+}
+
+// todo: put this in core folder
+internal interface ICombatant
+{
+    string? Name { get; set; }
+    int Strength { get; set; }
+    int Dexterity { get; set; }
+    int HitPoints { get; set; }
+    int Wins { get; set; }
+    int Losses { get; set; }
+}
+
+// todo: move to core folder
+internal class Transformer : ITransformer
+{
+    public string? Faction { get; set; }
+    public string? Name { get; set; }
+    public int Strength { get; set; }
+    public int Dexterity { get; set; }
+    public int HitPoints { get; set; }
+    public int Wins { get; set; }
+    public int Losses { get; set; }
+}
+
+// todo: add to Core folder
+internal interface ITransformerService
+{
+    ITransformer Create(string name, string faction);
+}
+
+// todo: move to Core folder
+internal class TransformerService : ITransformerService
+{
+    private readonly int _min = 1;
+    private readonly int _max = 20;
+
+    public TransformerService()
+    {
+    }
+
+    public ITransformer Create(string name, string faction)
+    {
+        name = name.Trim();
+
+        if (string.IsNullOrEmpty(name))
+        {
+            throw new Exception($"Expected {nameof(name)} but received '{name}'");
+        }
+
+        faction = faction.Trim();
+
+        if (string.IsNullOrEmpty(faction))
+        {
+            throw new Exception($"Expected {nameof(faction)} but received '{faction}");
+        }
+
+        var random = new Random();
+
+        return new Transformer()
+        {
+            Name = name,
+            Faction = faction,
+            Strength = random.Next(_min, _max),
+            Dexterity = random.Next(_min, _max),
+            HitPoints = random.Next(_min, _max)
+        };
+    }
 }
 
 public class Program
 {
+    private static readonly string _options = $"{Options.AddCombatant}: Add Combatant{Environment.NewLine}{Options.RemoveCombatant}: Remove Combatant{Environment.NewLine}{Options.SimulateBattle}: Simulate Battle{Environment.NewLine}{Options.CombatantStatistics}: Combatant Statistics{Environment.NewLine}{Options.Exit}: Exit";
+    private static bool _shouldContinue = true;
 
+    private static ITransformerService _transformerService;
 
-    private static readonly string options = $"{Options.Add_Combatant}: Add Combatant{Environment.NewLine}{Options.Remove_Combatant}: Remove Combatant{Environment.NewLine}{Options.Simulate_Battle}: Simulate Battle{Environment.NewLine}{Options.Combatant_Statistics}: Combatant Statistics{Environment.NewLine}{Options.Exit}: Exit";
-    private static bool shouldContinue = true;
+    public static bool IsValidValue(string? value)
+    {
+        value = value?.Trim();
 
-    public static void Main(string[] args)
+        return !string.IsNullOrEmpty(value);
+    }
+
+    static Program()
+    {
+        _transformerService = new TransformerService();
+    }
+
+    public static void Main()
     {
         Console.WriteLine("Welcome to Polemos!");
         Console.WriteLine("This is a battle simulation system");
         Console.WriteLine("Enter your name to get started:");
+
         var username = Console.ReadLine();
 
-        while (string.IsNullOrEmpty(username?.Trim()))
+        while (!IsValidValue(username))
         {
             Console.WriteLine("You must enter a username");
-            Console.WriteLine("Enter your name to get started:");
+            Console.WriteLine("Enter a username to get started:");
             username = Console.ReadLine();
         }
 
         Console.WriteLine($"Hello, {username}!");
-        Console.WriteLine("What would you like to do?");
-        Console.WriteLine(options);
-        var option = Console.ReadLine();
 
-        //todo: fix this so that strings convert to ints
-
-        while (shouldContinue)
+        while (_shouldContinue)
         {
-            switch (option)
+            Console.WriteLine("What would you like to do?");
+            Console.WriteLine(_options);
+            var option = Console.ReadLine();
+
+            int? value = null;
+            var isValidOption = false;
+
+            // todo: could refactor as private bool TryGetOption(string value, out var option)
+            while (!isValidOption)
             {
-                case Options.Add_Combatant:
+                try
+                {
+                    value = Convert.ToInt32(option);
+                    isValidOption = true;
+                }
+                catch
+                {
+                    Console.WriteLine($"'{option}' is not valid");
+                }
+            }
+
+            switch (value)
+            {
+                case Options.AddCombatant:
+                    Console.WriteLine("Enter a name:");
+                    var name = Console.ReadLine();
+
+                    while (!IsValidValue(name))
+                    {
+                        Console.WriteLine("You must enter a name");
+                        Console.WriteLine("Enter a name:");
+                        name = Console.ReadLine();
+                    }
+
+                    Console.WriteLine("Enter a faction:");
+
+                    var faction = Console.ReadLine();
+
+                    while (!IsValidValue(faction))
+                    {
+                        Console.WriteLine("You must enter a faction");
+                        Console.WriteLine("Enter a faction:");
+                        faction = Console.ReadLine();
+                    }
+
+                    var transformer = _transformerService.Create(name, faction);
+                    // todo: save to DbContext
+
+                    break;
+
+                case Options.RemoveCombatant:
                     // todo: implement
                     Console.WriteLine("Not implemented");
                     break;
 
-                case Options.Remove_Combatant:
+                case Options.SimulateBattle:
                     // todo: implement
                     Console.WriteLine("Not implemented");
                     break;
 
-                case Options.Simulate_Battle:
-                    // todo: implement
-                    Console.WriteLine("Not implemented");
-                    break;
-
-                case Options.Combatant_Statistics:
+                case Options.CombatantStatistics:
                     // todo: implement
                     Console.WriteLine("Not implemented");
                     break;
 
                 case Options.Exit:
-                    shouldContinue = false;
+                    _shouldContinue = false;
                     Console.WriteLine($"Goodbye, {username}!");
                     Console.WriteLine("Session ended");
                     break;
 
                 default:
-                    Console.WriteLine($"Sorry, {username}. '{option}' is not a supported option!");
-                    Console.WriteLine("Choose from the following options:");
-                    Console.WriteLine(options);
-                    option = Console.Read();
+                    Console.WriteLine($"Sorry, '{option}' is not a supported option");
                     break;
             }
         }
